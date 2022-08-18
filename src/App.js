3249bf42040dbea920ffe4e6ff2bf03bb535327b
LGS <cj8928@gmail.com>
2022-08-18 15:03:35 +0900

    마이페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-import Header from './components/Header';
+import Header from './layout/Header';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
-
+import Login from './pages/Login';
+import Mypage from './components/Mypage';
 function App() {
   return (
     <div>
       <Header />
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/signup' element={<Signup />} />
+      <Route path='/*' element={<Home />} />
+      <Route path='/member/new' element={<Signup />} />
+      <Route path='/member/login' element={<Login />} />
+      <Route path='/mypage' element={<Mypage />} />
     </Routes>
     </div>
   );
