1ccdb0f3383294ada6691c62eb1ba7255de2c285
LGS <cj8928@gmail.com>
2022-08-22 21:24:59 +0900

    챌린지 세부페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Mypage from './components/Mypage';
+import Challenge from './pages/Challenge';
+import ChallengeUpload from './pages/ChallengeUpload';
+import ChallengeDetail from './pages/ChallengeDetail';
+
+
 function App() {
   return (
     <div>
-      <Header />
-    <Routes>
-      <Route path='/*' element={<Home />} />
-      <Route path='/member/new' element={<Signup />} />
-      <Route path='/member/login' element={<Login />} />
-      <Route path='/mypage' element={<Mypage />} />
-    </Routes>
+        <Header />
+        <Routes>
+          <Route path='/*' element={<Home />} />
+          <Route path='/member/new' element={<Signup />} />
+          <Route path='/member/login' element={<Login />} />
+          <Route path='/mypage' element={<Mypage />} />
+          <Route path='/challenge/*' element={<Challenge />} />
+          <Route path='/challenge/upload' element={<ChallengeUpload />}/>
+          <Route path='/challenge/:id' element={<ChallengeDetail />}/>
+        </Routes>
     </div>
   );
 }
