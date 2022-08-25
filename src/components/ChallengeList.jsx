c1df9170d11fbe50ff4df40a15954980d09cccc1
LGS <cj8928@gmail.com>
2022-08-25 23:07:32 +0900

    주석 추가

--- src/components/ChallengeList.jsx
+++ src/components/ChallengeList.jsx
@@ -1,8 +1,15 @@
-import { Button, Card } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Card, ListGroup } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import './ChallengeList.css'
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function ChallengeList(props){
   const navigate = useNavigate();
+  /**
+   * props로 Challenge.jsx에서 챌린지 데이터 받아와서 출력
+   */
   const {id, 
   c_title,
   c_detail,
@@ -14,28 +21,36 @@ export default function ChallengeList(props){
   c_state,
   c_challengers} = props.list
 
-  function navigateDetail(){
+/**
+ * 챌린지 세부페이지로 이동
+ */
+  const navigateDetail=()=>{
     navigate(`/challenge/${id}`)
   }
 
+
   return(
-    <Card style={{width: '18rem'}}>
+    <Card className="challengecard" onClick={navigateDetail}>
       <Card.Img variant="top" />
       <Card.Body>
         <Card.Title>{c_title}</Card.Title>
         <Card.Text>
-          {c_detail}
+          {c_detail.substr(0,21)}
         </Card.Text>
-        <Card.Text>{c_recommnedataion}</Card.Text>
-        <Card.Text>{c_price}원</Card.Text>
-        <Card.Text>{c_donation_destination}</Card.Text>
-        <Card.Text>
-          {c_startTime} ~ {c_endTime}
-        </Card.Text>
-        <Card.Text>{c_state} {c_challengers}명</Card.Text>
-        <Button onClick={navigateDetail} variant="primary">챌린지 신청하기</Button>
       </Card.Body>
-
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>{c_recommnedataion}회</ListGroup.Item>
+          <ListGroup.Item>{c_price}원</ListGroup.Item>
+          <ListGroup.Item>{c_donation_destination}</ListGroup.Item>
+          <ListGroup.Item>{c_startTime} ~ {c_endTime}</ListGroup.Item>
+          <ListGroup.Item>{c_state}</ListGroup.Item>
+          <ListGroup.Item>{c_challengers}명</ListGroup.Item>
+        </ListGroup>
+        <Card.Body>
+          <FontAwesomeIcon icon={faThumbsUp}/>
+          <Button className="recommend">추천</Button>
+        </Card.Body>
+      
     </Card>
   )
 }
