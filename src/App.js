08ab8425233be3a03404a815d566bf41a061c9e7
LGS <cj8928@gmail.com>
2022-09-09 17:05:58 +0900

    라우터추가

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Header from './layout/Header';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import Mypage from './components/Mypage';
-import Challenge from './pages/Challenge';
-import ChallengeUpload from './pages/ChallengeUpload';
-import ChallengeDetail from './pages/ChallengeDetail';
+import Mypage from './pages//mypage/Mypage';
+import Challenge from './pages/challengepage/Challenge';
+import ChallengeUpload from './pages/challengepage/ChallengeUpload';
+import ChallengeDetail from './pages/challengepage/ChallengeDetail';
 import Footer from './layout/Footer';
+
 
 
 function App() {
@@ -19,8 +20,8 @@ function App() {
           <Route path='/*' element={<Home />} />
           <Route path='/member/new' element={<Signup />} />
           <Route path='/member/login' element={<Login />} />
-          <Route path='/mypage' element={<Mypage />} />
-          <Route path='/challenge/list' element={<Challenge />} />
+          <Route path='/mypage/*' element={<Mypage />} />
+          <Route path='/challenge/list/*' element={<Challenge />} />
           <Route path='/challenge/upload' element={<ChallengeUpload />}/>
           <Route path='/challenge/:id' element={<ChallengeDetail />}/>
         </Routes>
